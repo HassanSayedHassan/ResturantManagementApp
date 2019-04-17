a929943045eb6266eec7134806bbd98b322060f5
Soumik Bhattacharjee <soumikcse07@gmail.com>
2019-04-17 23:53:46 +0600

    splashScreen Update

--- app/src/main/java/com/example/cateringplatform/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/cateringplatform/activity/SplashScreenActivity.kt
@@ -1,20 +1,40 @@
 package com.example.cateringplatform.activity
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
 import android.view.WindowManager
 import com.example.cateringplatform.R
+import com.example.cateringplatform.Utills.hideProgressBar
+import com.example.cateringplatform.Utills.showProgressBar
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 1000 // 1 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.screen_splash)
 
 
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
     }
+
+
 }
